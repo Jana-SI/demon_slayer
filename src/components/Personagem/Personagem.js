3461c5af0e273a634799c8d58ed219b0df30cb82
Jana-SI <janaina.adolfo.ds@gmail.com>
2024-09-20 20:20:00 -0300

    add do css no container

--- src/components/Personagem/Personagem.js
+++ src/components/Personagem/Personagem.js
@@ -4,8 +4,8 @@ const Personagem = ({personagem}) => {
   return (
     <li className="personagemItem">
       <h3>{personagem.nome}</h3>
-      <p>{personagem.titulo}</p>
       <img src={personagem.imagem} alt={personagem.descricao_imagem} />
+      <p>{personagem.titulo}</p>
     </li>
   );
 };
